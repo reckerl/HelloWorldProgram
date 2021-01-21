06d83d5dc22a1cbf1a0a98fe1103b39b94bf0729
reckerl <73431425+reckerl@users.noreply.github.com>
2021-01-21 10:41:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*Programmer:Leah Recker
+ * Date: January 12th 2021
+ * Purpose:Program that displays HelloWorld Prg. and Introduction. And also in spanish
+ * Class:CITP 180
+ * Assignment:Chapter 1 Programming Exercise # 1
+ */
+
+using System;
+using static System.Console;
+
+namespace HelloWorldProgram
+{
+    class HelloWorld
+    {
+        static void Main( )
+        {
+            WriteLine("Hello World!\n");
+            WriteLine("My name is Leah Recker!\n");
+            WriteLine("Mi nombre es Leah Recker!\n");
+            ReadKey();
+        }
+    }
+}
